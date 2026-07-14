097f0a561660805bfafb5b892ead5504d2c92d7e
Julia Llama <julia.persidskaia@gmail.com>
2026-07-14 12:28:09 +0300

    Add random picture to Todo app and update it every 10 min

--- todo_app/main.go
+++ todo_app/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"context"
 	"embed"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"text/template"
 	"time"
@@ -34,10 +37,32 @@ var todoList = []todo{
 	{ID: "3", Title: "Develop a second application that simply responds with \"pong 0\" to a GET request"},
 }
 
+var (
+	mu         sync.Mutex
+	refreshing bool
+)
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
+	}
+
+	volumeDir := os.Getenv("VOLUME_DIR")
+	if volumeDir == "" {
+		log.Fatal("Variable VOLUME_DIR is required")
+	}
+
+	imageName := "image.jpeg"
+	imagePath := volumeDir + imageName
+
+	_, err := os.Stat(imagePath)
+	if errors.Is(err, os.ErrNotExist) {
+		err = downloadAndReplaceImage(volumeDir, imageName)
+	}
+
+	if err != nil {
+		log.Fatalf("Failed download image on start")
 	}
 
 	tmpl := template.Must(template.ParseFS(files, "templates/index.tmpl"))
@@ -56,6 +81,38 @@ func main() {
 		if err := tmpl.Execute(w, data); err != nil {
 			log.Printf("render page: %v", err)
 		}
+	})
+	mux.HandleFunc("GET /image", func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+
+		info, err := os.Stat(imagePath)
+		if err != nil {
+			mu.Unlock()
+			http.Error(w, "image not found", http.StatusInternalServerError)
+			return
+		}
+
+		expired := time.Since(info.ModTime()) >= 10*time.Minute
+
+		if expired && !refreshing {
+			refreshing = true
+
+			go func() {
+				defer func() {
+					mu.Lock()
+					refreshing = false
+					mu.Unlock()
+				}()
+
+				if err := downloadAndReplaceImage(volumeDir, imageName); err != nil {
+					log.Printf("failed to refresh image: %v", err)
+				}
+			}()
+		}
+
+		mu.Unlock()
+
+		http.ServeFile(w, r, imagePath)
 	})
 
 	addr := fmt.Sprintf(":%s", port)
@@ -84,4 +141,26 @@ func main() {
 	if err := server.Shutdown(shutdownCtx); err != nil {
 		log.Fatal("Server forced to shutdown", "error", err)
 	}
+}
+
+func downloadAndReplaceImage(path string, fileName string) error {
+	tmp := path + ".tmp"
+
+	resp, err := http.Get("https://picsum.photos/1200")
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	file, err := os.Create(tmp)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	if _, err := io.Copy(file, resp.Body); err != nil {
+		return err
+	}
+
+	return os.Rename(tmp, path+fileName)
 }
